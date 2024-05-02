cca364f7577a71471bdb8eb7d16b3bbbb69b9603
HarambeJS <168577489+HarambeJS@users.noreply.github.com>
2024-05-02 01:40:08 -0400

    First Update

---
+++ helloWorld.js
@@ -0,0 +1,11 @@
+let myVar = 'Hello World';
+
+console.log(myVar)
+
+let helloWorld = (anyString) => {
+    return anyString 
+}
+
+helloWorld('Hello World')
+
+module.exports.helloWorld = helloWorld()
