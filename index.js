54bfca92bb17695f5295eac63af8235069e08a13
ORENGER <168577489+HarambeJS@users.noreply.github.com>
2024-06-02 00:55:57 -0400

    adding to-do comments to index.js

--- index.js
+++ index.js
@@ -4,10 +4,11 @@ const { token } = require("./config.json");
 
 // Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+// To-do: add additional intents later
 
-// When the client is ready, run this code (only once).
-// The distinction between `client: Client<boolean>` and `readyClient: Client<true>` is important for TypeScript developers.
-// It makes some properties non-nullable.
+
+// Confirms client is logged in and online.
+// DOES NOT NEED TO BE CALLED FROM CLI AGAIN
 client.once(Events.ClientReady, (readyClient) => {
   console.log(`Ready! Logged in as ${readyClient.user.tag}`);
 });
