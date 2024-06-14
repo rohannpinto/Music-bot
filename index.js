e2f353a3208014ef7bb1ab76a2f80cfdd2d3d58e
HarambeJS <rohanpinto314@gmail.com>
2024-06-13 23:06:44 -0400

    further fixing project file structure

--- index.js
+++ index.js
@@ -4,11 +4,10 @@ const { token } = require("./config.json");
 
 // Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
-// To-do: add additional intents later
 
-
-// Confirms client is logged in and online.
-// DOES NOT NEED TO BE CALLED FROM CLI AGAIN
+// When the client is ready, run this code (only once).
+// The distinction between `client: Client<boolean>` and `readyClient: Client<true>` is important for TypeScript developers.
+// It makes some properties non-nullable.
 client.once(Events.ClientReady, (readyClient) => {
   console.log(`Ready! Logged in as ${readyClient.user.tag}`);
 });
