845dd04be84c49fbf0fe1d9e9571458c8a5e1b85
HarambeJS <168577489+HarambeJS@users.noreply.github.com>
2024-06-16 14:47:15 -0400

    Removing unnecessary comments

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const { token } = require("./config.json");
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 // When the client is ready, run this code (only once).
-// The distinction between `client: Client<boolean>` and `readyClient: Client<true>` is important for TypeScript developers.
 // It makes some properties non-nullable.
 client.once(Events.ClientReady, (readyClient) => {
   console.log(`Ready! Logged in as ${readyClient.user.tag}`);
