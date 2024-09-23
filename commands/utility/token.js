25bab965d0b79e4e099b8556020457ae3c89e290
HarambeJS <rohanpinto314@gmail.com>
2024-09-22 23:28:40 -0400

    Adding Intents and fixing Reaction Collector

--- commands/utility/token.js
+++ commands/utility/token.js
@@ -1,16 +1,14 @@
-//this command will DM a user the access token if user with role 'Disc Gorilla' approves a token request message in a server voice channel. The bot will also give the user the role 'Disc Monkey'
-//review AI code
-
 const { SlashCommandBuilder } = require("discord.js");
-//const {token,} = require("C:UsersRohanDocumentsCoding ProjectsHarambeBotconfig.json");
 const fs = require("node:fs");
 const path = require("node:path");
 
-const absoluteFilePath = path.join(path.dirname(path.dirname(__dirname)), 'config.json');
-const configData = fs.readFileSync(absoluteFilePath, 'utf8');
+const absoluteFilePath = path.join(
+  path.dirname(path.dirname(__dirname)),
+  "config.json"
+);
+const configData = fs.readFileSync(absoluteFilePath, "utf8");
 const config = JSON.parse(configData);
 const token = config.token;
-
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -19,36 +17,42 @@ module.exports = {
       "Request an access token with approval from a Disc Gorilla."
     ),
   async execute(interaction) {
-    const member = interaction.member;
-    const guild = interaction.guild;
-
+    const member = interaction.member; // No need to fetch, interaction.member is already a GuildMember object.
+    const guild = interaction.guild; // interaction.guild is already available.
+    const discGorillaRole = guild.roles.cache.find(
+      (role) => role.name === "Disc Gorilla"
+    );
     // Send the access token request message
     const message = await interaction.reply({
-      content:
-        "Requesting an access token. A @Disc Gorilla needs to approve this.",
+      content: `Requesting an access token. A <@&${discGorillaRole.id}> needs to approve this.`,
       fetchReply: true,
     });
 
+    // React to the message with an emoji for approval
+    await message.react("👍"); // Add a reaction for 'approval' (you can customize the emoji)
+
     // Create a reaction collector
-    const filter = (reaction, user) => {
-      // Check if the user who reacted has the 'Disc Gorilla' role
-      const hasRole = guild.members.cache
-        .get(user.id)
-        .roles.cache.some((role) => role.name === "Disc Gorilla");
-      return hasRole; // Collect reactions only if the user has the 'Disc Gorilla' role
+    const filter = async (reaction, user) => {
+      if (reaction.emoji.name !== "👍") return false; // Only collect 'thumbs up' reactions
+
+      // Fetch the member to ensure we're getting the latest roles
+      const reactingMember = await guild.members.fetch(user.id);
+      return reactingMember.roles.cache.some(
+        (role) => role.name === "Disc Gorilla"
+      ); // Check if the user has the Disc Gorilla role
     };
 
     const collector = message.createReactionCollector({
       filter,
-      max: 1,
-      time: 60000,
+      time: 60000, // 1 minute
     });
 
     collector.on("collect", async (reaction, user) => {
-      // Send the access token as a DM to the user who ran the /token command
       try {
-        const accessToken = token; // Generate or define your access token
-        await member.send(`Your access token is: ${accessToken}`);
+        // Send the access token as a DM to the user who requested it
+        await member.send(
+          `Your access token is: ${token}. Open Kenku FM's settings menu and paste the token in. [Visual Guide](https://www.kenku.fm/static/media/joinChannel.664b4edbddb0db7c0aba.mp4)`
+        );
 
         // Find or create the 'Disc Monkey' role and assign it to the user
         let role = guild.roles.cache.find(
@@ -61,7 +65,7 @@ module.exports = {
           });
         }
 
-        await member.roles.add(role); // Assign the role to the user
+        await member.roles.add(role); // Assign the role to the requesting user
         await interaction.followUp(
           `You have been granted the Disc Monkey role, and an access token has been sent to you!`
         );
--- index.js
+++ index.js
@@ -7,7 +7,15 @@ const { Client, Collection, Events, GatewayIntentBits } = require("discord.js");
 const { token } = require("./config.json");
 
 // Create a new client instance
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.GuildMessageReactions,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.DirectMessages,
+  ],
+});
 
 //allow commands to be accessed across files
 client.commands = new Collection();
